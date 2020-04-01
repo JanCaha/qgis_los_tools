22d9074062b391bc9732f08725742a7c9fb971c2
Jan Caha <jan.caha@outlook.com>
2020-04-01 16:46:16 +0200

    test suite change

--- los_tools/package_test_suite.py
+++ los_tools/package_test_suite.py
@@ -1,12 +1,15 @@
 import unittest
 import os
 
+from los_tools.test.test_tool_limit_angles_vector import LimitAnglesAlgorithmTest
+
 
 def test_package():
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
 
-    suite.addTests(loader.discover("test"))
+    # suite.addTests(loader.discover("test"))
+    suite.addTests(loader.loadTestsFromTestCase(LimitAnglesAlgorithmTest))
 
     runner = unittest.TextTestRunner(verbosity=3)
     result = runner.run(suite)
