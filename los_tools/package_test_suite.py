fed3c8d671fd82f6f44eb6dae4b208b89d1ea065
Jan Caha <jan.caha@outlook.com>
2021-01-15 20:32:58 +0100

    Revert "copy from r_processing plugin"
    This reverts commit 8d519a08a51693dbb3d3192639106038843cd27d.

--- los_tools/package_test_suite.py
+++ los_tools/package_test_suite.py
@@ -1,64 +1,19 @@
-import sys
-import os
 import unittest
-import tempfile
-from osgeo import gdal
-import qgis  # pylint: disable=unused-import
-
-try:
-    from pip import main as pipmain
-except ImportError:
-    from pip._internal import main as pipmain
-
-try:
-    import coverage
-except ImportError:
-    pipmain(['install', 'coverage'])
-    import coverage
 
 
-def _run_tests(test_suite, package_name, with_coverage=False):
-    """Core function to test a test suite."""
-    count = test_suite.countTestCases()
-    print('########')
-    print('%s tests has been discovered in %s' % (count, package_name))
-    print('Python GDAL : %s' % gdal.VersionInfo('VERSION_NUM'))
-    print('########')
-    if with_coverage:
-        cov = coverage.Coverage(
-            source=['/los_tools'],
-            omit=['*/test/*'],
-        )
-        cov.start()
+def test_package():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
 
-    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(test_suite)
+    suite.addTests(loader.discover("test"))
 
-    if with_coverage:
-        cov.stop()
-        cov.save()
-        report = tempfile.NamedTemporaryFile(delete=False)
-        cov.report(file=report)
-        # Produce HTML reports in the `htmlcov` folder and open index.html
-        # cov.html_report()
-        report.close()
-        with open(report.name, 'r') as fin:
-            print(fin.read())
+    print("################################")
+    print("Tests for LOS TOOLS Plugin runned from the Test Suite.")
+    print("%s tests has been found." % (suite.countTestCases()))
+    print("################################")
 
-
-def test_package(package='los_tools'):
-    test_loader = unittest.defaultTestLoader
-    try:
-        test_suite = test_loader.discover(package)
-    except ImportError:
-        test_suite = unittest.TestSuite()
-    _run_tests(test_suite, package)
-
-
-def test_environment():
-    package = os.environ.get('TESTING_PACKAGE', 'los_tools')
-    test_loader = unittest.defaultTestLoader
-    test_suite = test_loader.discover(package)
-    _run_tests(test_suite, package)
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
 
 
 if __name__ == '__main__':
