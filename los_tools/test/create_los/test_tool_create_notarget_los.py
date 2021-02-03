ecfe66f43873972b6d15c6ed602433b5e8d69b1d
Jan Caha <jan.caha@outlook.com>
2021-02-03 11:20:01 +0100

    fix tests

--- los_tools/test/create_los/test_tool_create_notarget_los.py
+++ los_tools/test/create_los/test_tool_create_notarget_los.py
@@ -53,10 +53,10 @@ class CreateNoTargetLosAlgorithmTest(QgsProcessingAlgorithmTestCase):
                                           parameter_type="field",
                                           parent_parameter="TargetPoints")
         self.assertQgsProcessingParameter(self.alg.parameterDefinition("LineDensity"),
-                                          parameter_type="number",
+                                          parameter_type="distance",
                                           default_value=1)
         self.assertQgsProcessingParameter(self.alg.parameterDefinition("MaxLoSLength"),
-                                          parameter_type="number",
+                                          parameter_type="distance",
                                           default_value=0)
         self.assertQgsProcessingParameter(self.alg.parameterDefinition("OutputLayer"),
                                           parameter_type="sink")
