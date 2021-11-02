2f7564a62f5aa4b5a3c44eb44ef75831b7e3cc85
Jan Caha <jan.caha@outlook.com>
2021-11-02 18:03:03 +0100

    new tools to calculate size of object at distances

---
+++ los_tools/tools/tool_sizes_at_distances.py
@@ -0,0 +1,121 @@
+import math
+
+from qgis.core import (
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterMatrix,
+    QgsProcessingParameterNumber,
+    QgsProcessingOutputString,
+    QgsProcessingFeedback,
+    QgsFields,
+    QgsField,
+    QgsWkbTypes,
+    QgsProcessingParameterFeatureSink,
+    QgsFeatureSink,
+    QgsFeature)
+
+from qgis.PyQt.QtCore import QVariant
+
+from los_tools.constants.field_names import FieldNames
+from .util_functions import log
+
+
+class ObjectSizesAlgorithm(QgsProcessingAlgorithm):
+
+    ANGLE = "Angle"
+    DISTANCES = "Distance"
+    OUTPUT_TABLE = "OutputTable"
+
+    def initAlgorithm(self, config=None):
+
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.ANGLE,
+                "Angle size of object (in degress)",
+                QgsProcessingParameterNumber.Double,
+                defaultValue=0.1,
+                minValue=0.0,
+                maxValue=100.0,
+                optional=False)
+        )
+
+        self.addParameter(
+            QgsProcessingParameterMatrix(
+                self.DISTANCES,
+                "Distances to calculate object size (in meters)",
+                numberRows=1,
+                headers=["Distance"],
+                defaultValue=[1000]
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT_TABLE,
+                "Output table")
+        )
+
+    def processAlgorithm(self, parameters, context, feedback: QgsProcessingFeedback):
+
+        angle = self.parameterAsDouble(parameters, self.ANGLE, context)
+        distances = self.parameterAsMatrix(parameters, self.DISTANCES, context)
+
+        fields = QgsFields()
+        fields.append(QgsField(FieldNames.SIZE_ANGLE, QVariant.Double))
+        fields.append(QgsField(FieldNames.DISTANCE, QVariant.Double))
+        fields.append(QgsField(FieldNames.SIZE, QVariant.Double))
+
+        sink, dest_id = self.parameterAsSink(parameters,
+                                             self.OUTPUT_TABLE,
+                                             context,
+                                             fields,
+                                             QgsWkbTypes.NoGeometry)
+
+        result_string_print = "Sizes at distances:\n" \
+                              "Distance - Size\n"
+
+        sizes = []
+        
+        angle = float(angle)
+        
+        for distance in distances:
+            
+            distance = float(distance)
+
+            size = round((math.tan(math.radians(angle))) * distance, 3)
+
+            result_string_print += f"{distance} - {size}\n"
+
+            sizes.append(size)
+
+            f = QgsFeature(fields)
+            f.setAttribute(f.fieldNameIndex(FieldNames.SIZE_ANGLE),
+                           float(angle))
+            f.setAttribute(f.fieldNameIndex(FieldNames.DISTANCE),
+                           float(distance))
+            f.setAttribute(f.fieldNameIndex(FieldNames.SIZE),
+                           float(size))
+
+            sink.addFeature(f)
+
+        feedback.pushInfo(result_string_print)
+
+        return {self.OUTPUT_TABLE: dest_id}
+
+    def name(self):
+        return "calculatesizes"
+
+    def displayName(self):
+        return "Calculate object sizes"
+
+    def group(self):
+        return "Object sizes"
+
+    def groupId(self):
+        return "objectsizes"
+
+    def createInstance(self):
+        return ObjectSizesAlgorithm()
+
+    def helpUrl(self):
+        pass
+        # return "https://jancaha.github.io/qgis_los_tools/tools/Points%20Creation/tool_points_around/"
---
+++ los_tools/tools/tools_distances_for_sizes.py
@@ -0,0 +1,116 @@
+import math
+
+from qgis.core import (
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterMatrix,
+    QgsProcessingParameterNumber,
+    QgsProcessingOutputString,
+    QgsProcessingFeedback,
+    QgsFields,
+    QgsField,
+    QgsWkbTypes,
+    QgsProcessingParameterFeatureSink,
+    QgsFeatureSink,
+    QgsFeature)
+
+from qgis.PyQt.QtCore import QVariant
+
+from los_tools.constants.field_names import FieldNames
+
+
+class ObjectDistancesAlgorithm(QgsProcessingAlgorithm):
+
+    ANGLE = "Angle"
+    SIZES = "Size"
+    OUTPUT_TABLE = "OutputTable"
+
+    def initAlgorithm(self, config=None):
+
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                self.ANGLE,
+                "Angle size of object (in degress)",
+                QgsProcessingParameterNumber.Double,
+                defaultValue=0.1,
+                minValue=0.0,
+                maxValue=100.0,
+                optional=False)
+        )
+
+        self.addParameter(
+            QgsProcessingParameterMatrix(
+                self.SIZES,
+                "Sizes of object to calculate (in meters)",
+                numberRows=1,
+                headers=["Size"],
+                defaultValue=[1]
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT_TABLE,
+                "Output table")
+        )
+
+    def processAlgorithm(self, parameters, context, feedback: QgsProcessingFeedback):
+
+        angle = self.parameterAsDouble(parameters, self.ANGLE, context)
+        sizes = self.parameterAsMatrix(parameters, self.SIZES, context)
+
+        fields = QgsFields()
+        fields.append(QgsField(FieldNames.SIZE_ANGLE, QVariant.Double))
+        fields.append(QgsField(FieldNames.DISTANCE, QVariant.Double))
+        fields.append(QgsField(FieldNames.SIZE, QVariant.Double))
+
+        sink, dest_id = self.parameterAsSink(parameters,
+                                             self.OUTPUT_TABLE,
+                                             context,
+                                             fields,
+                                             QgsWkbTypes.NoGeometry)
+
+        result_string_print = "Sizes at distances:\n" \
+                              "Size - Distance\n"
+
+        angle = float(angle)
+
+        for size in sizes:
+
+            size = float(size)
+
+            distance = round(size / math.tan(math.radians(angle)), 3)
+
+            result_string_print += f"{size} - {distance}\n"
+
+            f = QgsFeature(fields)
+            f.setAttribute(f.fieldNameIndex(FieldNames.SIZE_ANGLE),
+                           float(angle))
+            f.setAttribute(f.fieldNameIndex(FieldNames.DISTANCE),
+                           float(distance))
+            f.setAttribute(f.fieldNameIndex(FieldNames.SIZE),
+                           float(size))
+
+            sink.addFeature(f)
+
+        feedback.pushInfo(result_string_print)
+
+        return {self.OUTPUT_TABLE: dest_id}
+
+    def name(self):
+        return "calculatedistances"
+
+    def displayName(self):
+        return "Calculate object distances"
+
+    def group(self):
+        return "Object distances"
+
+    def groupId(self):
+        return "objectsizes"
+
+    def createInstance(self):
+        return ObjectDistancesAlgorithm()
+
+    def helpUrl(self):
+        pass
+        # return "https://jancaha.github.io/qgis_los_tools/tools/Points%20Creation/tool_points_around/"
