db484e9299d636f95de09db7bbbd9d411844fedd
Jan Caha <jan.caha@outlook.com>
2022-01-08 23:23:56 +0100

    new tool

---
+++ los_tools/tools/tool_extract_los_visibility_polygons.py
@@ -0,0 +1,231 @@
+from qgis.core import (QgsProcessing, QgsProcessingAlgorithm, QgsProcessingParameterNumber,
+                       QgsProcessingParameterFeatureSource, QgsProcessingParameterFeatureSink,
+                       QgsProcessingParameterBoolean, QgsField, QgsFeature, QgsWkbTypes, QgsFields,
+                       QgsVectorLayer, QgsFeatureIterator, QgsProcessingUtils, QgsMapLayer,
+                       QgsSymbol, QgsRendererCategory, QgsCategorizedSymbolRenderer,
+                       QgsMultiLineString, QgsLineString, QgsPoint, QgsPolygon, QgsMultiPolygon,
+                       QgsPointXY, QgsGeometry)
+
+from qgis.PyQt.QtCore import QVariant, Qt
+from los_tools.constants.field_names import FieldNames
+from los_tools.constants.textlabels import TextLabels
+from los_tools.classes.classes_los import LoSLocal, LoSGlobal, LoSWithoutTarget
+from los_tools.tools.util_functions import log, wkt_to_array_points, get_los_type
+from los_tools.constants.names_constants import NamesConstants
+
+
+class ExtractLoSVisibilityPolygonsAlgorithm(QgsProcessingAlgorithm):
+
+    LOS_LAYER = "LoSLayer"
+    OUTPUT_LAYER = "OutputLayer"
+    CURVATURE_CORRECTIONS = "CurvatureCorrections"
+    REFRACTION_COEFFICIENT = "RefractionCoefficient"
+
+    def initAlgorithm(self, config=None):
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(self.LOS_LAYER, "LoS layer",
+                                                [QgsProcessing.TypeVectorLine]))
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(self.OUTPUT_LAYER, "Output LoS parts layer"))
+
+        self.addParameter(
+            QgsProcessingParameterBoolean(self.CURVATURE_CORRECTIONS,
+                                          "Use curvature corrections?",
+                                          defaultValue=True))
+
+        self.addParameter(
+            QgsProcessingParameterNumber(self.REFRACTION_COEFFICIENT,
+                                         "Refraction coefficient value",
+                                         type=QgsProcessingParameterNumber.Double,
+                                         defaultValue=0.13))
+
+    def checkParameterValues(self, parameters, context):
+
+        los_layer = self.parameterAsVectorLayer(parameters, self.LOS_LAYER, context)
+
+        field_names = los_layer.fields().names()
+
+        if FieldNames.LOS_TYPE not in field_names:
+
+            msg = "Fields specific for LoS not found in current layer ({0}). " \
+                  "Cannot extract horizons from this layer.".format(FieldNames.LOS_TYPE)
+
+            return False, msg
+
+        return super().checkParameterValues(parameters, context)
+
+    def postProcessAlgorithm(self, context, feedback):
+
+        output_layer: QgsMapLayer = QgsProcessingUtils.mapLayerFromString(self.dest_id, context)
+
+        symbols = []
+
+        symbol_invisible = QgsSymbol.defaultSymbol(QgsWkbTypes.PolygonGeometry)
+        symbol_invisible.setColor(Qt.red)
+
+        symbol_invisible.symbolLayer(0).setStrokeStyle(Qt.PenStyle(Qt.NoPen))
+        symbols.append(QgsRendererCategory(False, symbol_invisible, TextLabels.INVISIBLE))
+
+        symbol_visible = QgsSymbol.defaultSymbol(QgsWkbTypes.PolygonGeometry)
+        symbol_visible.setColor(Qt.green)
+        symbol_visible.symbolLayer(0).setStrokeStyle(Qt.PenStyle(Qt.NoPen))
+        symbols.append(QgsRendererCategory(True, symbol_visible, TextLabels.VISIBLE))
+
+        renderer = QgsCategorizedSymbolRenderer(FieldNames.VISIBLE, symbols)
+
+        output_layer.setRenderer(renderer)
+
+        return {self.OUTPUT_LAYER: self.dest_id}
+
+    def processAlgorithm(self, parameters, context, feedback):
+
+        los_layer: QgsVectorLayer = self.parameterAsVectorLayer(parameters, self.LOS_LAYER,
+                                                                context)
+        curvature_corrections: bool = self.parameterAsBool(parameters, self.CURVATURE_CORRECTIONS,
+                                                           context)
+        ref_coeff: float = self.parameterAsDouble(parameters, self.REFRACTION_COEFFICIENT, context)
+
+        field_names = los_layer.fields().names()
+
+        los_type = get_los_type(los_layer, field_names)
+
+        fields = QgsFields()
+        fields.append(QgsField(FieldNames.ID_OBSERVER, QVariant.Int))
+        fields.append(QgsField(FieldNames.ID_TARGET, QVariant.Int))
+        fields.append(QgsField(FieldNames.VISIBLE, QVariant.Bool))
+
+        sink, self.dest_id = self.parameterAsSink(parameters, self.OUTPUT_LAYER, context, fields,
+                                                  QgsWkbTypes.MultiPolygon, los_layer.sourceCrs())
+
+        feature_count = los_layer.featureCount()
+
+        los_iterator: QgsFeatureIterator = los_layer.getFeatures()
+
+        for feature_number, los_feature in enumerate(los_iterator):
+
+            if feedback.isCanceled():
+                break
+
+            if los_type == NamesConstants.LOS_NO_TARGET:
+                los = LoSWithoutTarget(wkt_to_array_points(los_feature.geometry().asWkt()),
+                                       observer_offset=los_feature.attribute(
+                                           FieldNames.OBSERVER_OFFSET),
+                                       use_curvature_corrections=curvature_corrections,
+                                       refraction_coefficient=ref_coeff)
+
+            previous_point_visibility = True
+
+            polygon_multi_visible = QgsMultiPolygon()
+            polygon_multi_invisible = QgsMultiPolygon()
+
+            observer_point = QgsPointXY(los_feature.attribute(FieldNames.OBSERVER_X),
+                                        los_feature.attribute(FieldNames.OBSERVER_Y))
+
+            angle_width = los_feature.attribute(FieldNames.ANGLE_STEP)
+
+            line: QgsLineString = QgsLineString()
+
+            def line_to_polygon(line: QgsLineString, observer_point: QgsPointXY,
+                                angle_width: float) -> QgsPolygon:
+
+                angle_width = angle_width / 2
+
+                line_start_point = QgsPointXY(line.startPoint())
+                line_end_point = QgsPointXY(line.endPoint())
+
+                azimuth = observer_point.azimuth(line_end_point)
+
+                point_1 = observer_point.project(observer_point.distance(line_start_point),
+                                                 round(azimuth + angle_width, 2))
+                point_2 = observer_point.project(observer_point.distance(line_end_point),
+                                                 round(azimuth + angle_width, 2))
+                point_3 = observer_point.project(observer_point.distance(line_end_point),
+                                                 round(azimuth - angle_width, 2))
+                point_4 = observer_point.project(observer_point.distance(line_start_point),
+                                                 round(azimuth - angle_width, 2))
+
+                poly = QgsPolygon(
+                    QgsLineString(
+                        [line_start_point, point_1, point_2, line_end_point, point_3, point_4]))
+
+                # poly = poly.snappedToGrid(0.000001, 0.000001)
+
+                return poly
+
+            for i in range(0, len(los.points)):
+
+                line.addVertex(los.get_geom_at_index(i))
+
+                if los.visible[i] != previous_point_visibility:
+
+                    polygon = line_to_polygon(line, observer_point, angle_width)
+
+                    if polygon.isValid() and 1 < line.vertexCount():
+
+                        if previous_point_visibility:
+                            polygon_multi_visible.addGeometry(
+                                line_to_polygon(line, observer_point, angle_width))
+                        else:
+                            polygon_multi_invisible.addGeometry(
+                                line_to_polygon(line, observer_point, angle_width))
+
+                    line: QgsLineString = QgsLineString()
+                    line.addVertex(los.get_geom_at_index(i))
+                    previous_point_visibility = los.visible[i]
+
+                if i == len(los.points) - 1:
+
+                    polygon = line_to_polygon(line, observer_point, angle_width)
+
+                    if polygon.isValid() and 1 < line.vertexCount():
+
+                        if los.visible[i]:
+                            polygon_multi_visible.addGeometry(
+                                line_to_polygon(line, observer_point, angle_width))
+                        else:
+                            polygon_multi_invisible.addGeometry(
+                                line_to_polygon(line, observer_point, angle_width))
+
+            feature_visible = QgsFeature(fields)
+            feature_visible.setGeometry(polygon_multi_visible)
+            feature_visible.setAttribute(FieldNames.VISIBLE, True)
+            feature_visible.setAttribute(FieldNames.ID_OBSERVER,
+                                         los_feature.attribute(FieldNames.ID_OBSERVER))
+            feature_visible.setAttribute(FieldNames.ID_TARGET,
+                                         los_feature.attribute(FieldNames.ID_TARGET))
+
+            feature_invisible = QgsFeature(fields)
+            feature_invisible.setGeometry(polygon_multi_invisible)
+            feature_invisible.setAttribute(FieldNames.VISIBLE, False)
+            feature_invisible.setAttribute(FieldNames.ID_OBSERVER,
+                                           los_feature.attribute(FieldNames.ID_OBSERVER))
+            feature_invisible.setAttribute(FieldNames.ID_TARGET,
+                                           los_feature.attribute(FieldNames.ID_TARGET))
+
+            sink.addFeature(feature_visible)
+            sink.addFeature(feature_invisible)
+
+            feedback.setProgress((feature_number / feature_count) * 100)
+
+        return {self.OUTPUT_LAYER: self.dest_id}
+
+    def name(self):
+        return "extractvisibilitypolygonslos"
+
+    def displayName(self):
+        return "Extract Visibility Polygons from LoS"
+
+    def group(self):
+        return "LoS Points"
+
+    def groupId(self):
+        return "points"
+
+    def createInstance(self):
+        return ExtractLoSVisibilityPolygonsAlgorithm()
+
+    def helpUrl(self):
+        # FIXME add help page
+        return ""  # "https://jancaha.github.io/qgis_los_tools/tools/LoS%20Points/tool_extract_points_los/"
