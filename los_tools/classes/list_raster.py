3d96ee1c335d41b04e31d8c244a116fc298b76b0
Jan Caha <jan.caha@outlook.com>
2022-01-11 18:55:48 +0100

    extract raster list and sampling distance matrix into objects

---
+++ los_tools/classes/list_raster.py
@@ -0,0 +1,89 @@
+from typing import List, Tuple, Optional
+import math
+
+from qgis.core import (QgsMapLayer, QgsRasterLayer, QgsCoordinateReferenceSystem,
+                       QgsRasterDataProvider, QgsRectangle, QgsPoint)
+
+from los_tools.tools.util_functions import bilinear_interpolated_value
+
+
+class ListOfRasters:
+
+    rasters_dp: List[QgsRasterDataProvider]
+
+    def __init__(self, rasters: List[QgsMapLayer]):
+
+        for raster in rasters:
+            if not isinstance(raster, QgsRasterLayer):
+                raise ValueError("All inputs must be QgsRasterLayer.")
+
+        self.rasters = rasters
+
+        self.order_by_pixel_size()
+
+    def validate_bands(self) -> Tuple[bool, str]:
+
+        for raster in self.rasters:
+
+            dem_band_count = raster.bandCount()
+
+            if dem_band_count != 1:
+
+                msg = f"`{raster.name()}` can only have one band. Currently there are `{dem_band_count}` bands."
+
+                return False, msg
+
+        return True, ""
+
+    def validate_crs(self, crs: QgsCoordinateReferenceSystem) -> Tuple[bool, str]:
+
+        for raster in self.rasters:
+
+            if not raster.crs() == crs:
+
+                msg = f"`Observers point layer` and `{raster.name()}` crs must be equal. " \
+                    f"Right now they are not."
+
+                return False, msg
+
+        return True, ""
+
+    def order_by_pixel_size(self) -> None:
+
+        tuples = []
+
+        for raster in self.rasters:
+
+            tuples.append((raster, raster.extent().width() / raster.width()))
+
+        sorted_by_cell_size = sorted(tuples, key=lambda tup: tup[1])
+
+        self.rasters = [x[0] for x in sorted_by_cell_size]
+        self.rasters_dp = [x[0].dataProvider() for x in sorted_by_cell_size]
+
+    def maximal_diagonal_size(self) -> float:
+
+        extent: QgsRectangle = None
+
+        for raster in self.rasters:
+
+            if extent is None:
+
+                extent = raster.extent()
+
+            else:
+
+                extent.combineExtentWith(raster.extent())
+
+        return math.sqrt(math.pow(extent.width(), 2) + math.pow(extent.height(), 2))
+
+    def extract_interpolated_value(self, point: QgsPoint) -> Optional[float]:
+
+        for raster_dp in self.rasters_dp:
+
+            value = bilinear_interpolated_value(raster_dp, point)
+
+            if value is not None:
+                return value
+
+        return None
---
+++ los_tools/classes/sampling_distance_matrix.py
@@ -0,0 +1,160 @@
+from typing import List, Union
+
+import numpy as np
+
+from qgis.core import (QgsVectorLayer, QgsFeature, QgsPoint, QgsGeometry, QgsLineString)
+
+from los_tools.constants.field_names import FieldNames
+
+
+class SamplingDistanceMatrix:
+
+    NUMBER_OF_COLUMNS = 2
+
+    INDEX_SAMPLING_DISTANCE = 0
+    INDEX_DISTANCE = 1
+
+    def __init__(self, data: QgsVectorLayer):
+
+        self.data = []
+
+        feature: QgsFeature
+
+        for feature in data.getFeatures():
+
+            self.data.append(
+                [feature.attribute(FieldNames.SIZE),
+                 feature.attribute(FieldNames.DISTANCE)])
+
+        self.sort_data()
+
+    def __repr__(self):
+
+        strings = ["Sampling distance, Distance Limit"]
+
+        for row in self.data:
+
+            strings.append(f"{row[0]}, {row[1]}")
+
+        return "\n".join(strings)
+
+    def __len__(self):
+        return len(self.data)
+
+    def sort_data(self):
+        self.data = sorted(self.data, key=lambda d: d[self.INDEX_DISTANCE])
+
+    def replace_minus_one_with_value(self, value: float) -> None:
+
+        sampling_distance_limit = self.data[0][self.INDEX_SAMPLING_DISTANCE]
+
+        for row in self.data:
+            if value < row[self.INDEX_DISTANCE]:
+                sampling_distance_limit = row[self.INDEX_SAMPLING_DISTANCE]
+                break
+
+        if self.minimum_distance == -1:
+
+            self.data[0][self.INDEX_DISTANCE] = value
+            self.data[0][self.INDEX_SAMPLING_DISTANCE] = sampling_distance_limit
+
+            if 1 < len(self.data):
+                while self.data[0][self.INDEX_DISTANCE] < self.data[-1][self.INDEX_DISTANCE]:
+                    self.data.remove(self.data[-1])
+
+            self.sort_data()
+
+    def get_row(self, index: int) -> List[float]:
+        return self.data[index]
+
+    def get_row_distance(self, index: int):
+        return self.get_row(index)[self.INDEX_DISTANCE]
+
+    def get_row_sampling_distance(self, index: int):
+        return self.get_row(index)[self.INDEX_SAMPLING_DISTANCE]
+
+    @staticmethod
+    def validate_table(data: QgsVectorLayer):
+
+        field_names = data.fields().names()
+
+        if FieldNames.SIZE_ANGLE not in field_names:
+            return False, f"Field `{FieldNames.SIZE_ANGLE}` does not exist but is required."
+
+        if FieldNames.DISTANCE not in field_names:
+            return False, f"Field `{FieldNames.DISTANCE}` does not exist but is required."
+
+        if FieldNames.SIZE not in field_names:
+            return False, f"Field `{FieldNames.SIZE}` does not exist but is required."
+
+        if data.featureCount() < 1:
+            return False, "There are no features in the sampling distance - distance table (layer)."
+
+        return True, ""
+
+    @property
+    def minimum_distance(self) -> float:
+        return self.data[0][self.INDEX_DISTANCE]
+
+    @property
+    def maximum_distance(self) -> float:
+        return self.data[-1][self.INDEX_DISTANCE]
+
+    def next_distance(self, current_distance: float) -> float:
+
+        value_to_add = 0.0
+
+        row: List[float]
+
+        for row in self.data:
+            if current_distance < row[self.INDEX_DISTANCE]:
+                value_to_add = row[self.INDEX_SAMPLING_DISTANCE]
+
+        return current_distance + value_to_add
+
+    def build_line(self, origin_point: QgsPoint, direction_point: QgsPoint) -> QgsLineString:
+
+        line: Union[QgsLineString, QgsGeometry]
+
+        lines = []
+
+        for i in range(len(self)):
+
+            if i == 0:
+
+                line = QgsGeometry(
+                    QgsLineString([
+                        origin_point,
+                        origin_point.project(self.get_row_distance(i),
+                                             origin_point.azimuth(direction_point))
+                    ]))
+
+                line = line.densifyByDistance(
+                    distance=np.nextafter(self.get_row_sampling_distance(i), np.Inf))
+
+                line_res = QgsLineString()
+                line_res.fromWkt(line.asWkt())
+
+                lines.append(line_res)
+
+            else:
+
+                this_line: QgsLineString = lines[-1].clone()
+                this_line.extend(0, self.get_row_distance(i) - self.get_row_distance(i - 1))
+                this_line = QgsLineString([lines[-1].endPoint(), this_line.endPoint()])
+
+                line = QgsGeometry(this_line)
+                line = line.densifyByDistance(
+                    distance=np.nextafter(self.get_row_sampling_distance(i), np.Inf))
+
+                line_res = QgsLineString()
+                line_res.fromWkt(line.asWkt())
+
+                lines.append(line_res)
+
+        result_line = QgsLineString()
+
+        for line_part in lines:
+            result_line.append(line_part)
+
+        return result_line
