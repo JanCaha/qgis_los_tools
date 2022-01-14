c6dd4fd8be340e20a4f48fd192e0db4b0c09f139
Jan Caha <jan.caha@outlook.com>
2022-01-14 16:25:39 +0000

    update test

--- tests/tools/test_tool_extract_los_visibility_parts.py
+++ tests/tools/test_tool_extract_los_visibility_parts.py
@@ -1,14 +1,11 @@
-from qgis.core import (QgsVectorLayer,
-                       QgsWkbTypes)
+from qgis.core import (QgsVectorLayer, QgsWkbTypes)
 
 from los_tools.tools.tool_extract_los_visibility_parts import ExtractLoSVisibilityPartsAlgorithm
 from los_tools.constants.field_names import FieldNames
-from los_tools.constants.names_constants import NamesConstants
 
 from tests.AlgorithmTestCase import QgsProcessingAlgorithmTestCase
 
-from tests.utils_tests import (get_data_path,
-                               get_data_path_results)
+from tests.utils_tests import (get_data_path, get_data_path_results)
 
 
 class ExtractPointsLoSAlgorithmTest(QgsProcessingAlgorithmTestCase):
@@ -27,6 +24,7 @@ class ExtractPointsLoSAlgorithmTest(QgsProcessingAlgorithmTestCase):
         self.alg.initAlgorithm()
 
     def test_parameters(self) -> None:
+
         self.assertQgsProcessingParameter(self.alg.parameterDefinition("LoSLayer"),
                                           parameter_type="source")
         self.assertQgsProcessingParameter(self.alg.parameterDefinition("OutputLayer"),
@@ -38,17 +36,18 @@ class ExtractPointsLoSAlgorithmTest(QgsProcessingAlgorithmTestCase):
                                           parameter_type="number",
                                           default_value=0.13)
 
+    def test_alg_settings(self) -> None:
+
+        self.assertAlgSettings()
+
     def test_check_wrong_params(self) -> None:
 
         # use layer that is not correctly constructed LoS layer
-        params = {
-            "LoSLayer": QgsVectorLayer(get_data_path(file="no_target_los_wrong.gpkg"))
-        }
+        params = {"LoSLayer": QgsVectorLayer(get_data_path(file="no_target_los_wrong.gpkg"))}
 
         self.assertCheckParameterValuesRaisesMessage(
             parameters=params,
-            message="Fields specific for LoS not found in current layer (los_type)."
-        )
+            message="Fields specific for LoS not found in current layer (los_type).")
 
     def test_run_alg(self) -> None:
 
@@ -69,9 +68,51 @@ class ExtractPointsLoSAlgorithmTest(QgsProcessingAlgorithmTestCase):
                                   geom_type=QgsWkbTypes.MultiLineStringZ,
                                   crs=self.los_local.sourceCrs())
 
-        self.assertFieldNamesInQgsVectorLayer([FieldNames.ID_OBSERVER,
-                                               FieldNames.ID_TARGET,
-                                               FieldNames.VISIBLE],
-                                              los_parts)
+        self.assertFieldNamesInQgsVectorLayer(
+            [FieldNames.ID_OBSERVER, FieldNames.ID_TARGET, FieldNames.VISIBLE], los_parts)
 
-        self.assertEqual(los_parts.featureCount(), self.los_local.featureCount()*2)
+        self.assertEqual(los_parts.featureCount(), self.los_local.featureCount() * 2)
+
+        output_path = get_data_path_results(file="los_parts.gpkg")
+
+        params = {
+            "LoSLayer": self.los_global,
+            "OutputLayer": output_path,
+            "CurvatureCorrections": True,
+            "RefractionCoefficient": 0.13
+        }
+
+        self.assertRunAlgorithm(parameters=params)
+
+        los_parts = QgsVectorLayer(output_path)
+
+        self.assertQgsVectorLayer(los_parts,
+                                  geom_type=QgsWkbTypes.MultiLineStringZ,
+                                  crs=self.los_local.sourceCrs())
+
+        self.assertFieldNamesInQgsVectorLayer(
+            [FieldNames.ID_OBSERVER, FieldNames.ID_TARGET, FieldNames.VISIBLE], los_parts)
+
+        self.assertEqual(los_parts.featureCount(), self.los_global.featureCount() * 2)
+
+        output_path = get_data_path_results(file="los_parts.gpkg")
+
+        params = {
+            "LoSLayer": self.los_no_target,
+            "OutputLayer": output_path,
+            "CurvatureCorrections": True,
+            "RefractionCoefficient": 0.13
+        }
+
+        self.assertRunAlgorithm(parameters=params)
+
+        los_parts = QgsVectorLayer(output_path)
+
+        self.assertQgsVectorLayer(los_parts,
+                                  geom_type=QgsWkbTypes.MultiLineStringZ,
+                                  crs=self.los_local.sourceCrs())
+
+        self.assertFieldNamesInQgsVectorLayer(
+            [FieldNames.ID_OBSERVER, FieldNames.ID_TARGET, FieldNames.VISIBLE], los_parts)
+
+        self.assertEqual(los_parts.featureCount(), self.los_no_target.featureCount() * 2)
