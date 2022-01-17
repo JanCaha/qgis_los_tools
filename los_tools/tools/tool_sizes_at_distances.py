b3f9b61eaf927bb4af00f155f02607dad0d6580c
Jan Caha <jan.caha@outlook.com>
2022-01-17 20:23:43 +0000

    change tool and update tests

--- los_tools/tools/tool_sizes_at_distances.py
+++ los_tools/tools/tool_sizes_at_distances.py
@@ -1,9 +1,9 @@
 import math
 
 from qgis.core import (QgsProcessingAlgorithm, QgsProcessingParameterMatrix,
-                       QgsProcessingParameterBoolean, QgsProcessingParameterNumber,
-                       QgsProcessingFeedback, QgsFields, QgsField, QgsWkbTypes,
-                       QgsProcessingParameterFeatureSink, QgsFeature)
+                       QgsProcessingParameterNumber, QgsProcessingParameterBoolean,
+                       QgsProcessingParameterNumber, QgsProcessingFeedback, QgsFields, QgsField,
+                       QgsWkbTypes, QgsProcessingParameterFeatureSink, QgsFeature)
 
 from qgis.PyQt.QtCore import QVariant
 
@@ -15,6 +15,7 @@ class ObjectSizesAlgorithm(QgsProcessingAlgorithm):
     ANGLE = "Angle"
     DISTANCES = "Distance"
     MAXIMALDISTANCE = "MaximalDistance"
+    DEFAULT_SAMPLING_DISTANCE = "DefaultSamplingDistance"
     OUTPUT_TABLE = "OutputTable"
 
     def initAlgorithm(self, config=None):
@@ -35,6 +36,14 @@ class ObjectSizesAlgorithm(QgsProcessingAlgorithm):
                                          headers=["Distance"],
                                          defaultValue=[1000]))
 
+        param = QgsProcessingParameterNumber(self.DEFAULT_SAMPLING_DISTANCE,
+                                             "Default sampling size (in meters)",
+                                             defaultValue=1,
+                                             type=QgsProcessingParameterNumber.Double)
+        param.setMetadata({'widget_wrapper': {'decimals': 3}})
+
+        self.addParameter(param)
+
         self.addParameter(
             QgsProcessingParameterBoolean(
                 self.MAXIMALDISTANCE,
@@ -43,11 +52,35 @@ class ObjectSizesAlgorithm(QgsProcessingAlgorithm):
 
         self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_TABLE, "Output table"))
 
+    def checkParameterValues(self, parameters, context):
+
+        distances = self.parameterAsMatrix(parameters, self.DISTANCES, context)
+
+        if len(distances) < 1:
+            msg = f"Legth of distances must be at least 1. It is {len(distances)}."
+
+            return False, msg
+
+        for distance in distances:
+
+            try:
+                value = float(distance)
+
+            except ValueError:
+
+                msg = f"Cannot convert value `{distance}` to float number."
+
+                return False, msg
+
+        return super().checkParameterValues(parameters, context)
+
     def processAlgorithm(self, parameters, context, feedback: QgsProcessingFeedback):
 
         angle = self.parameterAsDouble(parameters, self.ANGLE, context)
         distances = self.parameterAsMatrix(parameters, self.DISTANCES, context)
         maximal_distance = self.parameterAsBoolean(parameters, self.MAXIMALDISTANCE, context)
+        default_sampling_size = self.parameterAsDouble(parameters, self.DEFAULT_SAMPLING_DISTANCE,
+                                                       context)
 
         fields = QgsFields()
         fields.append(QgsField(FieldNames.SIZE_ANGLE, QVariant.Double))
@@ -63,6 +96,15 @@ class ObjectSizesAlgorithm(QgsProcessingAlgorithm):
         angle = float(angle)
 
         maximal_sampling_distance = 0
+
+        f = QgsFeature(fields)
+        f.setAttribute(f.fieldNameIndex(FieldNames.SIZE_ANGLE), float(angle))
+        f.setAttribute(f.fieldNameIndex(FieldNames.DISTANCE), 0)
+        f.setAttribute(f.fieldNameIndex(FieldNames.SIZE), default_sampling_size)
+
+        sink.addFeature(f)
+
+        result_string_print += f"{0} - {default_sampling_size}\n"
 
         for distance in distances:
 
--- tests/tools/test_tool_sizes_at_distances.py
+++ tests/tools/test_tool_sizes_at_distances.py
@@ -28,23 +28,53 @@ class ObjectSizesAlgorithmTest(QgsProcessingAlgorithmTestCase):
         self.assertQgsProcessingParameter(self.alg.parameterDefinition("OutputTable"),
                                           parameter_type="sink")
 
+        self.assertQgsProcessingParameter(self.alg.parameterDefinition("DefaultSamplingDistance"),
+                                          parameter_type="number")
+
+        self.assertQgsProcessingParameter(self.alg.parameterDefinition("MaximalDistance"),
+                                          parameter_type="boolean")
+
+    def test_alg_settings(self) -> None:
+
+        self.assertAlgSettings()
+
     def test_run_alg(self) -> None:
 
-        output_path = get_data_path_results(file="table.xlsx")
+        output_path = get_data_path_results(file="table.gpkg")
 
         params = {
-            "Angle": 0.1,
-            "OutputTable": output_path,
-            "Distance": ["1000", "2000", "3000", "4000", "5000"]
+            'Angle': 0.1,
+            'DefaultSamplingDistance': 1,
+            'Distance': [1000, 2000, 3000, 4000, 5000],
+            'MaximalDistance': True,
+            'OutputTable': output_path
         }
 
-        self.assertRunAlgorithm(parameters=params)
+        can_run, msg = self.alg.checkParameterValues(params, context=self.context)
+        self.assertTrue(can_run)
+        self.assertIn("", msg)
 
-        table = QgsVectorLayer(output_path)
+        self.alg.run(parameters=params, context=self.context, feedback=self.feedback)
+
+        table = QgsVectorLayer(f"{output_path}|layername=table")
 
         self.assertQgsVectorLayer(table, geom_type=QgsWkbTypes.NoGeometry, crs=None)
 
         self.assertFieldNamesInQgsVectorLayer(
             [FieldNames.SIZE_ANGLE, FieldNames.DISTANCE, FieldNames.SIZE], table)
 
-        self.assertEqual(table.featureCount(), 5)
+        self.assertEqual(len(table.allFeatureIds()), 7)
+
+        angles = []
+        distances = []
+        sampling_sizes = []
+
+        for feature in table.getFeatures():
+
+            angles.append(feature.attribute(FieldNames.SIZE_ANGLE))
+            distances.append(feature.attribute(FieldNames.DISTANCE))
+            sampling_sizes.append(feature.attribute(FieldNames.SIZE))
+
+        assert angles == [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+        assert distances == [0, 1000, 2000, 3000, 4000, 5000, -1]
+        assert sampling_sizes == [1.0, 1.745, 3.491, 5.236, 6.981, 8.727, 8.727]
