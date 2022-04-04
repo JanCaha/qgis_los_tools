58850382db25e8304a341926ef8f13821bb8fb65
Jan Caha <jan.caha@outlook.com>
2022-04-04 16:42:15 +0200

    tests for new tools

--- tests/tools/test_tool_replace_raster_values_by_constant.py
+++ tests/tools/test_tool_replace_raster_values_by_constant.py
@@ -3,12 +3,11 @@ import unittest
 from osgeo import gdal
 import numpy as np
 
-from qgis.core import (QgsRasterLayer, QgsProcessingParameterField, QgsProcessingFeedback,
-                       QgsProcessingContext)
+from qgis.core import (QgsRasterLayer, QgsProcessingFeedback, QgsProcessingContext)
 
 from processing.core.Processing import Processing
 
-from los_tools.tools.tool_replace_raster_values import ReplaceRasterValuesAlgorithm
+from los_tools.tools.tool_replace_raster_values_by_constant import ReplaceRasterValuesByConstantValueAlgorithm
 
 from tests.AlgorithmTestCase import QgsProcessingAlgorithmTestCase
 from tests.utils_tests import (print_alg_params, print_alg_outputs, get_data_path,
@@ -24,7 +23,7 @@ class ReplaceRasterValuesAlgorithmTest(QgsProcessingAlgorithmTestCase):
 
         self.output_path = get_data_path_results(file="raster_new_values.tif")
 
-        self.alg = ReplaceRasterValuesAlgorithm()
+        self.alg = ReplaceRasterValuesByConstantValueAlgorithm()
         self.alg.initAlgorithm()
 
         self.feedback = QgsProcessingFeedback()
@@ -46,18 +45,14 @@ class ReplaceRasterValuesAlgorithmTest(QgsProcessingAlgorithmTestCase):
         param_raster_layer = self.alg.parameterDefinition("RasterLayer")
         param_vector_layer = self.alg.parameterDefinition("VectorLayer")
         param_raster_value = self.alg.parameterDefinition("RasterValue")
-        param_value_field = self.alg.parameterDefinition("ValueField")
         param_output_raster = self.alg.parameterDefinition("OutputRaster")
 
         self.assertEqual("raster", param_raster_layer.type())
         self.assertEqual("source", param_vector_layer.type())
         self.assertEqual("number", param_raster_value.type())
-        self.assertEqual("field", param_value_field.type())
         self.assertEqual("rasterDestination", param_output_raster.type())
 
         self.assertEqual(1, param_raster_value.defaultValue())
-        self.assertEqual("VectorLayer", param_value_field.parentLayerParameterName())
-        self.assertEqual(QgsProcessingParameterField.Numeric, param_value_field.dataType())
 
     def test_check_wrong_params(self) -> None:
         # does not apply, there are no checks for parameters
@@ -71,7 +66,6 @@ class ReplaceRasterValuesAlgorithmTest(QgsProcessingAlgorithmTestCase):
             "RasterLayer": self.raster,
             "VectorLayer": self.polygons,
             "RasterValue": -100,
-            "ValueField": "",
             "OutputRaster": self.output_path,
         }
 
@@ -81,25 +75,5 @@ class ReplaceRasterValuesAlgorithmTest(QgsProcessingAlgorithmTestCase):
         raster_array = ds.GetRasterBand(1).ReadAsArray()
 
         self.assertIn(-100, np.unique(raster_array))
-
-        ds = None
-
-        params = {
-            "RasterLayer": self.raster,
-            "VectorLayer": self.polygons,
-            "RasterValue": -1,
-            "ValueField": "height",
-            "OutputRaster": self.output_path,
-        }
-
-        self.alg.run(parameters=params, context=self.context, feedback=self.feedback)
-
-        ds = gdal.Open(self.output_path)
-        raster_array = ds.GetRasterBand(1).ReadAsArray()
-        unique_values = np.unique(raster_array)
-
-        self.assertIn(1200, unique_values)
-        self.assertIn(1100, unique_values)
-        self.assertIn(1050, unique_values)
 
         ds = None
---
+++ tests/tools/test_tool_replace_raster_values_by_field.py
@@ -0,0 +1,84 @@
+import unittest
+
+from osgeo import gdal
+import numpy as np
+
+from qgis.core import (QgsRasterLayer, QgsProcessingParameterField, QgsProcessingFeedback,
+                       QgsProcessingContext)
+
+from processing.core.Processing import Processing
+
+from los_tools.tools.tool_replace_raster_values_by_field import ReplaceRasterValuesByFieldValuesAlgorithm
+
+from tests.AlgorithmTestCase import QgsProcessingAlgorithmTestCase
+from tests.utils_tests import (print_alg_params, print_alg_outputs, get_data_path,
+                               get_data_path_results)
+
+
+class ReplaceRasterValuesAlgorithmTest(QgsProcessingAlgorithmTestCase):
+
+    def setUp(self) -> None:
+
+        self.raster = QgsRasterLayer(get_data_path(file="dsm.tif"))
+        self.polygons = f'{get_data_path(file="polys.gpkg")}|layername=polys'
+
+        self.output_path = get_data_path_results(file="raster_new_values.tif")
+
+        self.alg = ReplaceRasterValuesByFieldValuesAlgorithm()
+        self.alg.initAlgorithm()
+
+        self.feedback = QgsProcessingFeedback()
+        self.context = QgsProcessingContext()
+
+    @unittest.skip("printing not necessary `test_show_params()`")
+    def test_show_params(self) -> None:
+        print("{}".format(self.alg.name()))
+        print("----------------------------------")
+        print_alg_params(self.alg)
+        print("----------------------------------")
+        print_alg_outputs(self.alg)
+
+    def test_alg_settings(self) -> None:
+
+        self.assertAlgSettings()
+
+    def test_parameters(self) -> None:
+        param_raster_layer = self.alg.parameterDefinition("RasterLayer")
+        param_vector_layer = self.alg.parameterDefinition("VectorLayer")
+        param_value_field = self.alg.parameterDefinition("ValueField")
+        param_output_raster = self.alg.parameterDefinition("OutputRaster")
+
+        self.assertEqual("raster", param_raster_layer.type())
+        self.assertEqual("source", param_vector_layer.type())
+        self.assertEqual("field", param_value_field.type())
+        self.assertEqual("rasterDestination", param_output_raster.type())
+
+        self.assertEqual("VectorLayer", param_value_field.parentLayerParameterName())
+        self.assertEqual(QgsProcessingParameterField.Numeric, param_value_field.dataType())
+
+    def test_check_wrong_params(self) -> None:
+        # does not apply, there are no checks for parameters
+        pass
+
+    def test_run_alg(self):
+
+        Processing.initialize()
+
+        params = {
+            "RasterLayer": self.raster,
+            "VectorLayer": self.polygons,
+            "ValueField": "height",
+            "OutputRaster": self.output_path,
+        }
+
+        self.alg.run(parameters=params, context=self.context, feedback=self.feedback)
+
+        ds = gdal.Open(self.output_path)
+        raster_array = ds.GetRasterBand(1).ReadAsArray()
+        unique_values = np.unique(raster_array)
+
+        self.assertIn(1200, unique_values)
+        self.assertIn(1100, unique_values)
+        self.assertIn(1050, unique_values)
+
+        ds = None
