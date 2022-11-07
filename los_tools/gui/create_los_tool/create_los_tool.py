e035f14796014daec0dc35adf922aace9003cb53
Jan Caha <jan.caha@outlook.com>
2022-11-07 21:18:30 +0100

    create los refactor

---
+++ los_tools/gui/create_los_tool/create_los_tool.py
@@ -0,0 +1,260 @@
+import numpy as np
+from functools import partial
+
+from qgis.PyQt.QtWidgets import (QWidget, QAction)
+from qgis.PyQt.QtCore import (Qt, pyqtSignal)
+from qgis.PyQt.QtGui import (QKeyEvent)
+from qgis.core import (QgsWkbTypes, QgsGeometry, QgsVectorLayer, QgsPointLocator, Qgis, QgsPoint,
+                       QgsPointXY, QgsVertexId)
+from qgis.gui import (QgisInterface, QgsMapMouseEvent, QgsSnapIndicator,
+                      QgsMapToolAdvancedDigitizing)
+
+from los_tools.classes import ListOfRasters
+from los_tools.gui import LoSSettings
+from los_tools.gui import RasterValidations
+from los_tools.tools.util_functions import get_max_decimal_numbers, round_all_values
+
+from .create_los_widget import LoSNoTargetInputWidget
+from .los_tasks import LoSExtractionTaskManager, PrepareLoSWithoutTargetTask, PrepareLoSTask
+
+
+class CreateLoSMapTool(QgsMapToolAdvancedDigitizing):
+
+    featuresAdded = pyqtSignal()
+
+    def __init__(self,
+                 iface: QgisInterface,
+                 raster_validation_dialog: RasterValidations,
+                 los_settings_dialog: LoSSettings,
+                 los_layer: QgsVectorLayer = None,
+                 add_result_action: QAction = None) -> None:
+
+        super().__init__(iface.mapCanvas(), iface.cadDockWidget())
+        self._iface = iface
+        self._canvas = self._iface.mapCanvas()
+
+        self.task_manager = LoSExtractionTaskManager()
+
+        self._los_layer = los_layer
+
+        self._raster_validation_dialog = raster_validation_dialog
+        self._los_settings_dialog = los_settings_dialog
+
+        self.add_result_action = add_result_action
+
+        self._start_point: QgsPointXY = None
+
+        self._last_towards_point: QgsPointXY = None
+
+        self._snapper = self._canvas.snappingUtils()
+        self.snap_marker = QgsSnapIndicator(self._canvas)
+
+        self._los_rubber_band = self.createRubberBand(QgsWkbTypes.LineGeometry)
+
+        self._widget: QWidget = None
+
+    def set_los_layer(self, layer: QgsVectorLayer) -> None:
+        self._los_layer = layer
+
+    def create_widget(self):
+        self.delete_widget()
+
+        self._widget = LoSNoTargetInputWidget()
+        self._iface.addUserInputWidget(self._widget)
+        self._widget.setFocus(Qt.TabFocusReason)
+
+        self._widget.valuesChanged.connect(partial(self.draw_los, None))
+        self._widget.saveToLayerClicked.connect(self.add_los_to_layer)
+
+    def delete_widget(self):
+        if self._widget:
+            self._widget.releaseKeyboard()
+            self._widget.deleteLater()
+            self._widget = None
+
+    def activate(self) -> None:
+        super(CreateLoSMapTool, self).activate()
+        self.create_widget()
+        self.messageDiscarded.emit()
+        self._canvas = self._iface.mapCanvas()
+        self._snapper = self._canvas.snappingUtils()
+        if self._canvas.mapSettings().destinationCrs().isGeographic():
+            self.messageEmitted.emit(
+                "Tool only works if canvas is in projected CRS. Currently canvas is in geographic CRS.",
+                Qgis.Critical)
+            self.hide_widgets()
+            self.deactivate()
+            return
+        if not ListOfRasters.validate(self._raster_validation_dialog.list_of_selected_rasters):
+            self.messageEmitted.emit("Tool needs valid setup in `Raster Validatations` dialog.",
+                                     Qgis.Critical)
+            self.deactivate()
+            return
+
+    def clean(self) -> None:
+        if self._widget:
+            self._widget.disableAddLos()
+        self.snap_marker.setVisible(False)
+        self._los_rubber_band.hide()
+        self._start_point = None
+
+    def deactivate(self) -> None:
+        self.clean()
+        self.delete_widget()
+        self._iface.mapCanvas().unsetMapTool(self)
+        super(CreateLoSMapTool, self).deactivate()
+
+    def canvasReleaseEvent(self, e: QgsMapMouseEvent) -> None:
+        if e.button() == Qt.RightButton:
+            self.clean()
+        elif e.button() == Qt.LeftButton:
+            if self._start_point is None:
+                if self._snap_point:
+                    self._start_point = self._snap_point
+                else:
+                    self._start_point = e.mapPoint()
+            else:
+                self.draw_los(self._snap_point)
+                self._widget.enableAddLos()
+                self._start_point = None
+
+    def canvasMoveEvent(self, event: QgsMapMouseEvent) -> None:
+        result = self._snapper.snapToMap(event.pos())
+        self.snap_marker.setMatch(result)
+        if result.type() == QgsPointLocator.Vertex:
+            self._snap_point = result.point()
+        else:
+            self._snap_point = event.mapPoint()
+
+        if self._start_point is not None:
+            self.draw_los(self._snap_point)
+
+    def keyPressEvent(self, e: QKeyEvent) -> None:
+        if e.key() == Qt.Key_Escape:
+            self.deactivate()
+            self._iface.mapCanvas().unsetMapTool(self)
+        return super().keyPressEvent(e)
+
+    def draw_los(self, towards_point: QgsPointXY):
+
+        if towards_point is None:
+            towards_point = self._last_towards_point
+
+        canvas_crs = self._canvas.mapSettings().destinationCrs()
+
+        if canvas_crs.isGeographic():
+            self._iface.messageBar().pushMessage(
+                "LoS can be drawn only for projected CRS. Canvas is currently in geographic CRS.",
+                Qgis.Critical,
+                duration=5)
+            return
+
+        if self._start_point and towards_point:
+
+            self._los_rubber_band.hide()
+
+            rasters_extent = self._raster_validation_dialog.listOfRasters.extent_polygon()
+
+            if self._widget.los_local or self._widget.los_global:
+
+                if self._widget.los_local:
+                    line = QgsGeometry.fromPolylineXY([self._start_point, towards_point])
+
+                if self._widget.los_global:
+                    line = QgsGeometry.fromPolylineXY([self._start_point, towards_point])
+
+                    if self._start_point.distance(towards_point) > 0:
+                        line = line.extendLine(
+                            0,
+                            self._raster_validation_dialog.listOfRasters.maximal_diagonal_size())
+
+                        # insert target point
+                        line.get().insertVertex(QgsVertexId(0, 0, 1),
+                                                QgsPoint(towards_point.x(), towards_point.y()))
+
+                line = line.intersection(rasters_extent)
+
+                self._los_rubber_band.setToGeometry(line,
+                                                    self._canvas.mapSettings().destinationCrs())
+
+            if self._widget.los_no_target:
+
+                self._los_rubber_band.setToGeometry(QgsGeometry(),
+                                                    self._canvas.mapSettings().destinationCrs())
+
+                maximal_length_distance = self._los_settings_dialog.los_maximal_length()
+
+                if maximal_length_distance is None:
+                    maximal_length = self._raster_validation_dialog.listOfRasters.maximal_diagonal_size(
+                    )
+                else:
+                    maximal_length = maximal_length_distance.inUnits(
+                        self._los_layer.crs().mapUnits())
+
+                angle = self._start_point.azimuth(towards_point)
+
+                angles = np.arange(
+                    angle - self._widget.angle_difference,
+                    angle + self._widget.angle_difference + 0.000000001 * self._widget.angle_step,
+                    self._widget.angle_step).tolist()
+                round_digits = get_max_decimal_numbers([
+                    angle - self._widget.angle_difference,
+                    angle + self._widget.angle_difference + 0.000000001 * self._widget.angle_step,
+                    self._widget.angle_step
+                ])
+                angles = round_all_values(angles, round_digits)
+                size_constant = 1
+                for angle in angles:
+                    new_point = self._start_point.project(size_constant, angle)
+                    line = QgsGeometry.fromPolylineXY([self._start_point, new_point])
+                    line = line.extendLine(0, maximal_length - size_constant)
+
+                    line = line.intersection(rasters_extent)
+
+                    self._los_rubber_band.addGeometry(line,
+                                                      self._canvas.mapSettings().destinationCrs())
+
+            self._los_rubber_band.show()
+
+        if towards_point:
+            self._last_towards_point = QgsPointXY(towards_point.x(), towards_point.y())
+
+    def set_result_action_active(self, active: bool) -> None:
+        if self.add_result_action:
+            self.add_result_action.setEnabled(active)
+
+    def add_los_to_layer(self) -> None:
+        los_geometry = self._los_rubber_band.asGeometry()
+
+        list_of_rasters = self._raster_validation_dialog.listOfRasters
+
+        # text = self.add_result_action.text()
+        # self.add_result_action.setText("Obtaining data ...")
+        self.set_result_action_active(False)
+
+        if los_geometry.get().partCount() == 1:
+
+            task = PrepareLoSTask(
+                los_geometry,
+                self._widget.sampling_distance.inUnits(self._los_layer.crs().mapUnits()),
+                self._los_layer, list_of_rasters, self._widget.observer_offset,
+                self._widget.target_offset, self._widget.los_global,
+                self._iface.mapCanvas().mapSettings().destinationCrs())
+
+        else:
+
+            task = PrepareLoSWithoutTargetTask(
+                los_geometry, self._los_layer, list_of_rasters, self._los_settings_dialog,
+                self._widget.observer_offset, self._widget.angle_step,
+                self._iface.mapCanvas().mapSettings().destinationCrs())
+
+        task.taskCompleted.connect(self.task_finished)
+
+        self.task_manager.addTask(task)
+
+    def task_finished(self) -> None:
+        self.featuresAdded.emit()
+        if self.task_manager.all_los_tasks_finished():
+            self.set_result_action_active(True)
+        self._iface.messageBar().pushMessage("LoS Without Target Processing Finished",
+                                             Qgis.MessageLevel.Info, 2)
---
+++ los_tools/gui/create_los_tool/create_los_widget.py
@@ -0,0 +1,145 @@
+from typing import Optional
+
+from qgis.PyQt.QtWidgets import (QWidget, QFormLayout, QComboBox, QPushButton)
+from qgis.PyQt.QtCore import (pyqtSignal)
+from qgis.gui import (QgsDoubleSpinBox)
+
+from los_tools.gui import Distance, DistanceWidget
+
+
+class LoSNoTargetInputWidget(QWidget):
+
+    valuesChanged = pyqtSignal()
+    saveToLayerClicked = pyqtSignal()
+
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        super().__init__(parent)
+
+        self.form_layout = QFormLayout()
+        self.form_layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.form_layout)
+
+        self._los_type = QComboBox(self)
+        self._los_type.addItem("Local")
+        self._los_type.addItem("Global")
+        self._los_type.addItem("No Target")
+        self._los_type.currentIndexChanged.connect(self.change_setting_enabled)
+
+        self._observers_offset = QgsDoubleSpinBox(self)
+        self._observers_offset.setMinimum(0.0)
+        self._observers_offset.setMaximum(999999.999)
+        self._observers_offset.setValue(1.6)
+        self._observers_offset.setClearValue(1.6)
+
+        self._target_offset = QgsDoubleSpinBox(self)
+        self._target_offset.setMinimum(0.0)
+        self._target_offset.setMaximum(999999.999)
+        self._target_offset.setValue(0.0)
+        self._target_offset.setClearValue(0.0)
+
+        self._sampling_distance = DistanceWidget(self)
+        self._sampling_distance.setMinimum(0.01)
+        self._sampling_distance.setMaximum(999999.999)
+        self._sampling_distance.setValue(1.0)
+        self._sampling_distance.setClearValue(1.0)
+        self._sampling_distance.setDecimals(2)
+
+        self._angle_difference = QgsDoubleSpinBox(self)
+        self._angle_difference.setMinimum(0.000)
+        self._angle_difference.setMaximum(180.000)
+        self._angle_difference.setValue(10.000)
+        self._angle_difference.setClearValue(10.000)
+        self._angle_difference.setDecimals(3)
+        self._angle_difference.setEnabled(False)
+        self._angle_difference.setSuffix("°")
+        self._angle_difference.valueChanged.connect(self.emit_values_changed)
+
+        self._angle_step = QgsDoubleSpinBox(self)
+        self._angle_step.setMinimum(0.001)
+        self._angle_step.setMaximum(90)
+        self._angle_step.setValue(1)
+        self._angle_step.setClearValue(1)
+        self._angle_step.setDecimals(3)
+        self._angle_step.setEnabled(False)
+        self._angle_step.setSuffix("°")
+        self._angle_step.valueChanged.connect(self.angle_step_changed)
+        self._angle_step.valueChanged.connect(self.emit_values_changed)
+
+        self._add_los_to_layer = QPushButton("Add LoS to Plugin Layer")
+        self._add_los_to_layer.setEnabled(False)
+        self._add_los_to_layer.clicked.connect(self.clickedAddLosToLayer)
+
+        self.form_layout.addRow("LoS Type", self._los_type)
+        self.form_layout.addRow("Observer Offset", self._observers_offset)
+        self.form_layout.addRow("Target Offset", self._target_offset)
+        self.form_layout.addRow("Sampling Distance", self._sampling_distance)
+        self.form_layout.addRow("Angle Difference", self._angle_difference)
+        self.form_layout.addRow("Angle Step", self._angle_step)
+        self.form_layout.addWidget(self._add_los_to_layer)
+
+    def change_setting_enabled(self) -> None:
+        self._angle_difference.setEnabled(self.los_no_target)
+        self._angle_step.setEnabled(self.los_no_target)
+        if self.los_local or self.los_global:
+            self._angle_difference.setEnabled(False)
+            self._angle_step.setEnabled(False)
+            self._sampling_distance.setEnabled(True)
+            self._target_offset.setEnabled(True)
+        else:
+            self._angle_difference.setEnabled(True)
+            self._angle_step.setEnabled(True)
+            self._sampling_distance.setEnabled(False)
+            self._target_offset.setEnabled(False)
+
+    def angle_step_changed(self) -> None:
+        if self._angle_step.value() > self._angle_difference.value() * 2:
+            self._angle_step.setValue(self._angle_difference.value() * 2)
+
+    @property
+    def sampling_distance(self) -> Distance:
+        return self._sampling_distance.distance()
+
+    @property
+    def observer_offset(self) -> float:
+        return self._observers_offset.value()
+
+    @property
+    def target_offset(self) -> float:
+        return self._target_offset.value()
+
+    @property
+    def angle_step(self) -> float:
+        return self._angle_step.value()
+
+    @property
+    def angle_difference(self) -> float:
+        return self._angle_difference.value()
+
+    @property
+    def los_type(self) -> str:
+        return self._los_type.currentText()
+
+    @property
+    def los_local(self) -> bool:
+        return self._los_type.currentText() == "Local"
+
+    @property
+    def los_global(self) -> bool:
+        return self._los_type.currentText() == "Global"
+
+    @property
+    def los_no_target(self) -> bool:
+        return self._los_type.currentText() == "No Target"
+
+    def emit_values_changed(self) -> None:
+        self.valuesChanged.emit()
+
+    def clickedAddLosToLayer(self) -> None:
+        self.saveToLayerClicked.emit()
+        self.disableAddLos()
+
+    def enableAddLos(self) -> None:
+        self._add_los_to_layer.setEnabled(True)
+
+    def disableAddLos(self) -> None:
+        self._add_los_to_layer.setEnabled(False)
