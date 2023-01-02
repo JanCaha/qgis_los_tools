9f510283fc580a9b97ddd1730daf0b6dd986158d
Jan Caha <jan.caha@outlook.com>
2023-01-02 20:31:50 +0100

    sample using this plugin implementation

--- los_tools/constants/__init__.py
+++ los_tools/constants/__init__.py
@@ -3,5 +3,6 @@ from .fields import Fields
 from .names_constants import NamesConstants
 from .plugin import PluginConstants
 from .textlabels import TextLabels
+from .settings import Settings
 
-__all__ = ("FieldNames", "Fields", "NamesConstants", "PluginConstants", "TextLabels")
+__all__ = ("FieldNames", "Fields", "NamesConstants", "PluginConstants", "TextLabels", "Settings")
