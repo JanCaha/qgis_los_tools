0094139beec301731ccb4f21b5b33615438bfbfb
Jan Caha <jan.caha@outlook.com>
2024-07-04 14:34:20 +0200

    update to new API

--- los_tools/constants/fields.py
+++ los_tools/constants/fields.py
@@ -1,21 +1,21 @@
-from qgis.PyQt.QtCore import QVariant
-from qgis.core import (QgsFields, QgsField)
+from qgis.core import QgsField, QgsFields
+from qgis.PyQt.QtCore import QMetaType
 
 from .field_names import FieldNames
 
 
 class Fields:
 
-    _field_azimuth = QgsField(FieldNames.AZIMUTH, QVariant.Double)
-    _field_angle_step = QgsField(FieldNames.ANGLE_STEP, QVariant.Double)
-    _field_observer_x = QgsField(FieldNames.OBSERVER_X, QVariant.Double)
-    _field_observer_y = QgsField(FieldNames.OBSERVER_Y, QVariant.Double)
+    _field_azimuth = QgsField(FieldNames.AZIMUTH, QMetaType.Type.Double)
+    _field_angle_step = QgsField(FieldNames.ANGLE_STEP, QMetaType.Type.Double)
+    _field_observer_x = QgsField(FieldNames.OBSERVER_X, QMetaType.Type.Double)
+    _field_observer_y = QgsField(FieldNames.OBSERVER_Y, QMetaType.Type.Double)
 
     _base_fields = QgsFields()
-    _base_fields.append(QgsField(FieldNames.LOS_TYPE, QVariant.String))
-    _base_fields.append(QgsField(FieldNames.ID_OBSERVER, QVariant.Int))
-    _base_fields.append(QgsField(FieldNames.ID_TARGET, QVariant.Int))
-    _base_fields.append(QgsField(FieldNames.OBSERVER_OFFSET, QVariant.Double))
+    _base_fields.append(QgsField(FieldNames.LOS_TYPE, QMetaType.Type.QString))
+    _base_fields.append(QgsField(FieldNames.ID_OBSERVER, QMetaType.Type.Int))
+    _base_fields.append(QgsField(FieldNames.ID_TARGET, QMetaType.Type.Int))
+    _base_fields.append(QgsField(FieldNames.OBSERVER_OFFSET, QMetaType.Type.Double))
 
     los_notarget_fields = QgsFields(_base_fields)
     los_notarget_fields.append(_field_azimuth)
@@ -24,11 +24,11 @@ class Fields:
     los_notarget_fields.append(_field_angle_step)
 
     los_local_fields = QgsFields(_base_fields)
-    los_local_fields.append(QgsField(FieldNames.TARGET_OFFSET, QVariant.Double))
+    los_local_fields.append(QgsField(FieldNames.TARGET_OFFSET, QMetaType.Type.Double))
 
     los_global_fields = QgsFields(los_local_fields)
-    los_global_fields.append(QgsField(FieldNames.TARGET_X, QVariant.Double))
-    los_global_fields.append(QgsField(FieldNames.TARGET_Y, QVariant.Double))
+    los_global_fields.append(QgsField(FieldNames.TARGET_X, QMetaType.Type.Double))
+    los_global_fields.append(QgsField(FieldNames.TARGET_Y, QMetaType.Type.Double))
 
     los_plugin_layer_fields = QgsFields(los_global_fields)
     los_plugin_layer_fields.append(_field_azimuth)
