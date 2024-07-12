9bec30435385a4008f6d0ac260e3dc51961aac09
Jan Caha <jan.caha@outlook.com>
2024-07-12 21:38:28 +0200

    update

--- tests/processing/parameter_settings/test_tool_sizes_at_distances.py
+++ tests/processing/parameter_settings/test_tool_sizes_at_distances.py
@@ -1,80 +1,74 @@
-from qgis.core import QgsVectorLayer, QgsWkbTypes
+from qgis.core import Qgis, QgsVectorLayer
 
-from los_tools.processing.parameter_settings.tool_sizes_at_distances import ObjectSizesAlgorithm
 from los_tools.constants.field_names import FieldNames
+from los_tools.processing.parameter_settings.tool_sizes_at_distances import ObjectSizesAlgorithm
+from tests.custom_assertions import (
+    assert_algorithm,
+    assert_check_parameter_values,
+    assert_field_names_exist,
+    assert_layer,
+    assert_parameter,
+    assert_run,
+)
+from tests.utils import result_filename
 
-from tests.AlgorithmTestCase import QgsProcessingAlgorithmTestCase
-from tests.utils_tests import get_data_path_results
+
+def test_parameters() -> None:
+    alg = ObjectSizesAlgorithm()
+    alg.initAlgorithm()
+
+    assert_parameter(alg.parameterDefinition("Angle"), parameter_type="number")
+
+    assert_parameter(alg.parameterDefinition("Distance"), parameter_type="matrix")
+
+    assert_parameter(alg.parameterDefinition("OutputTable"), parameter_type="sink")
+
+    assert_parameter(alg.parameterDefinition("DefaultSamplingDistance"), parameter_type="number")
+
+    assert_parameter(alg.parameterDefinition("MaximalDistance"), parameter_type="boolean")
 
 
-class ObjectSizesAlgorithmTest(QgsProcessingAlgorithmTestCase):
+def test_alg_settings() -> None:
+    alg = ObjectSizesAlgorithm()
+    alg.initAlgorithm()
 
-    def setUp(self) -> None:
+    assert_algorithm(alg)
 
-        super().setUp()
 
-        self.alg = ObjectSizesAlgorithm()
+def test_run_alg() -> None:
+    alg = ObjectSizesAlgorithm()
+    alg.initAlgorithm()
 
-        self.alg.initAlgorithm()
+    output_path = result_filename("table.gpkg")
 
-    def test_parameters(self) -> None:
+    params = {
+        "Angle": 0.1,
+        "DefaultSamplingDistance": 1,
+        "Distance": [1000, 2000, 3000, 4000, 5000],
+        "MaximalDistance": True,
+        "OutputTable": output_path,
+    }
 
-        self.assertQgsProcessingParameter(self.alg.parameterDefinition("Angle"),
-                                          parameter_type="number")
+    assert_run(alg, params)
 
-        self.assertQgsProcessingParameter(self.alg.parameterDefinition("Distance"),
-                                          parameter_type="matrix")
+    table = QgsVectorLayer(f"{output_path}|layername=table")
 
-        self.assertQgsProcessingParameter(self.alg.parameterDefinition("OutputTable"),
-                                          parameter_type="sink")
+    assert_layer(table, geom_type=Qgis.WkbType.NoGeometry, crs=None)
 
-        self.assertQgsProcessingParameter(self.alg.parameterDefinition("DefaultSamplingDistance"),
-                                          parameter_type="number")
+    assert_field_names_exist([FieldNames.SIZE_ANGLE, FieldNames.DISTANCE, FieldNames.SIZE], table)
 
-        self.assertQgsProcessingParameter(self.alg.parameterDefinition("MaximalDistance"),
-                                          parameter_type="boolean")
+    assert len(table.allFeatureIds()) == 7
 
-    def test_alg_settings(self) -> None:
+    angles = []
+    distances = []
+    sampling_sizes = []
 
-        self.assertAlgSettings()
+    for feature in table.getFeatures():
 
-    def test_run_alg(self) -> None:
+        angles.append(feature.attribute(FieldNames.SIZE_ANGLE))
+        distances.append(feature.attribute(FieldNames.DISTANCE))
+        sampling_sizes.append(feature.attribute(FieldNames.SIZE))
 
-        output_path = get_data_path_results(file="table.gpkg")
-
-        params = {
-            'Angle': 0.1,
-            'DefaultSamplingDistance': 1,
-            'Distance': [1000, 2000, 3000, 4000, 5000],
-            'MaximalDistance': True,
-            'OutputTable': output_path
-        }
-
-        can_run, msg = self.alg.checkParameterValues(params, context=self.context)
-        self.assertTrue(can_run)
-        self.assertIn("", msg)
-
-        self.alg.run(parameters=params, context=self.context, feedback=self.feedback)
-
-        table = QgsVectorLayer(f"{output_path}|layername=table")
-
-        self.assertQgsVectorLayer(table, geom_type=QgsWkbTypes.NoGeometry, crs=None)
-
-        self.assertFieldNamesInQgsVectorLayer(
-            [FieldNames.SIZE_ANGLE, FieldNames.DISTANCE, FieldNames.SIZE], table)
-
-        self.assertEqual(len(table.allFeatureIds()), 7)
-
-        angles = []
-        distances = []
-        sampling_sizes = []
-
-        for feature in table.getFeatures():
-
-            angles.append(feature.attribute(FieldNames.SIZE_ANGLE))
-            distances.append(feature.attribute(FieldNames.DISTANCE))
-            sampling_sizes.append(feature.attribute(FieldNames.SIZE))
-
-        assert angles == [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-        assert distances == [0, 1000, 2000, 3000, 4000, 5000, -1]
-        assert sampling_sizes == [1.0, 1.745, 3.491, 5.236, 6.981, 8.727, 8.727]
+    assert angles == [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    assert distances == [0, 1000, 2000, 3000, 4000, 5000, -1]
+    assert sampling_sizes == [1.0, 1.745, 3.491, 5.236, 6.981, 8.727, 8.727]
