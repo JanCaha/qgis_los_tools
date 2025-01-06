29eea8afc73b9f2842f071164843ea713308764e
Jan Caha <jan.caha@outlook.com>
2025-01-06 17:17:37 +0100

    draw distance limits

--- los_tools/gui/los_without_target_visualization/los_without_target.py
+++ los_tools/gui/los_without_target_visualization/los_without_target.py
@@ -16,12 +16,15 @@ class LosNoTargetMapTool(LoSDigitizingToolWithWidget):
         self._widget = LoSNoTargetInputWidget()
         self._widget.hide()
 
+        self._distance_limits_rubber_band = self.createRubberBand(Qgis.GeometryType.Line)
+        self._distance_limits_rubber_band.setColor(Qt.GlobalColor.darkGreen)
+
     def create_widget(self):
         if not self._widget:
             self._widget = LoSNoTargetInputWidget()
         super().create_widget()
 
-        self._widget.valuesChanged.connect(self.draw_los)
+        self._widget.valuesChanged.connect(self.draw)
 
     def canvasMoveEvent(self, e: QgsMapMouseEvent) -> None:
         self._set_snap_point(e)
@@ -34,7 +37,18 @@ class LosNoTargetMapTool(LoSDigitizingToolWithWidget):
             self.clean()
         if e.button() == Qt.LeftButton:
             self._selected_point = e.mapPoint()
-            self.draw_los()
+            self.draw()
+
+    def draw(self) -> None:
+        self.draw_los()
+        if self._widget.show_distance_limits:
+            self.draw_limits()
+        else:
+            self._distance_limits_rubber_band.reset()
+
+    def clean(self) -> None:
+        super().clean()
+        self._distance_limits_rubber_band.reset()
 
     def draw_los(self):
 
@@ -66,3 +80,44 @@ class LosNoTargetMapTool(LoSDigitizingToolWithWidget):
                 geom = geom.extendLine(0, self._widget.length - size_constant)
                 self._los_rubber_band.addGeometry(geom, self._canvas.mapSettings().destinationCrs())
             self._los_rubber_band.show()
+
+    def draw_limits(self) -> None:
+
+        if self._snap_point:
+            self._selected_point = self._snap_point
+
+        if not self.canvas_crs_is_projected():
+            return
+
+        if self._selected_point:
+
+            self._distance_limits_rubber_band.reset()
+
+            angles = np.arange(
+                self._widget.min_angle,
+                self._widget.max_angle + 0.000000001 * self._widget.angle_step,
+                step=self._widget.angle_step,
+            ).tolist()
+
+            if self._widget.max_angle > 359:
+                angles.append(angles[0])
+
+            round_digits = get_max_decimal_numbers(
+                [
+                    self._widget.min_angle,
+                    self._widget.max_angle,
+                    self._widget.angle_step,
+                ]
+            )
+            angles = round_all_values(angles, round_digits)
+
+            for dist in self._widget.distance_limits:
+                line = []
+                for angle in angles:
+                    new_point = self._selected_point.project(dist, angle)
+                    line.append(new_point)
+
+                geom = QgsGeometry.fromPolylineXY(line)
+                self._distance_limits_rubber_band.addGeometry(geom, self._canvas.mapSettings().destinationCrs())
+
+            self._distance_limits_rubber_band.show()
