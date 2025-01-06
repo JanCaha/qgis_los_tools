36708c38e8d819d2cacf46fa3ecff8c3e9517978
Jan Caha <jan.caha@outlook.com>
2025-01-06 17:16:28 +0100

    save settings

--- los_tools/gui/create_los_tool/create_los_widget.py
+++ los_tools/gui/create_los_tool/create_los_widget.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
+from qgis.core import Qgis, QgsSettings, QgsUnitTypes
 from qgis.gui import QgsDoubleSpinBox
-from qgis.PyQt.QtCore import pyqtSignal
+from qgis.PyQt.QtCore import QSignalBlocker, pyqtSignal
 from qgis.PyQt.QtWidgets import QComboBox, QFormLayout, QPushButton, QWidget
 
+from los_tools.constants.plugin import PluginConstants
 from los_tools.gui.custom_classes import Distance, DistanceWidget
 
 
@@ -23,18 +25,21 @@ class LoSInputWidget(QWidget):
         self._los_type.addItem("Global")
         self._los_type.addItem("No Target")
         self._los_type.currentIndexChanged.connect(self.change_setting_enabled)
+        self._los_type.currentIndexChanged.connect(self.save_settings)
 
-        self._observers_offset = QgsDoubleSpinBox(self)
-        self._observers_offset.setMinimum(0.0)
-        self._observers_offset.setMaximum(999999.999)
-        self._observers_offset.setValue(1.6)
-        self._observers_offset.setClearValue(1.6)
+        self._observer_offset = QgsDoubleSpinBox(self)
+        self._observer_offset.setMinimum(0.0)
+        self._observer_offset.setMaximum(999999.999)
+        self._observer_offset.setValue(1.6)
+        self._observer_offset.setClearValue(1.6)
+        self._observer_offset.valueChanged.connect(self.save_settings)
 
         self._target_offset = QgsDoubleSpinBox(self)
         self._target_offset.setMinimum(0.0)
         self._target_offset.setMaximum(999999.999)
         self._target_offset.setValue(0.0)
         self._target_offset.setClearValue(0.0)
+        self._target_offset.valueChanged.connect(self.save_settings)
 
         self._sampling_distance = DistanceWidget(self)
         self._sampling_distance.setMinimum(0.01)
@@ -42,6 +47,7 @@ class LoSInputWidget(QWidget):
         self._sampling_distance.setValue(1.0)
         self._sampling_distance.setClearValue(1.0)
         self._sampling_distance.setDecimals(2)
+        self._sampling_distance.valueChanged.connect(self.save_settings)
 
         self._angle_difference = QgsDoubleSpinBox(self)
         self._angle_difference.setMinimum(0.000)
@@ -51,7 +57,8 @@ class LoSInputWidget(QWidget):
         self._angle_difference.setDecimals(3)
         self._angle_difference.setEnabled(False)
         self._angle_difference.setSuffix("°")
-        self._angle_difference.valueChanged.connect(self.emit_values_changed)
+        self._angle_difference.valueChanged.connect(self.valuesChanged.emit)
+        self._angle_difference.valueChanged.connect(self.save_settings)
 
         self._angle_step = QgsDoubleSpinBox(self)
         self._angle_step.setMinimum(0.001)
@@ -62,14 +69,15 @@ class LoSInputWidget(QWidget):
         self._angle_step.setEnabled(False)
         self._angle_step.setSuffix("°")
         self._angle_step.valueChanged.connect(self.angle_step_changed)
-        self._angle_step.valueChanged.connect(self.emit_values_changed)
+        self._angle_step.valueChanged.connect(self.valuesChanged.emit)
+        self._angle_step.valueChanged.connect(self.save_settings)
 
         self._add_los_to_layer = QPushButton("Add LoS to Plugin Layer")
         self._add_los_to_layer.setEnabled(False)
         self._add_los_to_layer.clicked.connect(self.clickedAddLosToLayer)
 
         self.form_layout.addRow("LoS Type", self._los_type)
-        self.form_layout.addRow("Observer Offset", self._observers_offset)
+        self.form_layout.addRow("Observer Offset", self._observer_offset)
         self.form_layout.addRow("Target Offset", self._target_offset)
         self.form_layout.addRow("Sampling Distance", self._sampling_distance)
         self.form_layout.addRow("Angle Difference", self._angle_difference)
@@ -100,7 +108,7 @@ class LoSInputWidget(QWidget):
 
     @property
     def observer_offset(self) -> float:
-        return self._observers_offset.value()
+        return self._observer_offset.value()
 
     @property
     def target_offset(self) -> float:
@@ -130,12 +138,77 @@ class LoSInputWidget(QWidget):
     def los_no_target(self) -> bool:
         return self._los_type.currentText() == "No Target"
 
-    def emit_values_changed(self) -> None:
-        self.valuesChanged.emit()
-
     def clickedAddLosToLayer(self) -> None:
         self.saveToLayerClicked.emit()
         self.setAddLoSEnabled(False)
 
     def setAddLoSEnabled(self, enabled: bool) -> None:
         self._add_los_to_layer.setEnabled(enabled)
+
+    def load_settings(self) -> None:
+        settings = QgsSettings()
+        settings_class = f"{PluginConstants.settings_group}/CreateLoSTool"
+
+        with QSignalBlocker(self._los_type):
+            self._los_type.setCurrentText(
+                settings.value(f"{settings_class}/LoSType", "Local", type=str, section=QgsSettings.Section.Plugins)
+            )
+
+        with QSignalBlocker(self._observer_offset):
+            self._observer_offset.setValue(
+                settings.value(f"{settings_class}/ObserverOffset", 1.6, type=float, section=QgsSettings.Section.Plugins)
+            )
+
+        with QSignalBlocker(self._target_offset):
+
+            self._target_offset.setValue(
+                settings.value(f"{settings_class}/TargetOffset", 0, type=float, section=QgsSettings.Section.Plugins)
+            )
+
+        with QSignalBlocker(self._sampling_distance):
+            self._sampling_distance.setValue(
+                settings.value(f"{settings_class}/SamplingDistance", 1, type=float, section=QgsSettings.Section.Plugins)
+            )
+
+        unit, success = QgsUnitTypes.decodeDistanceUnit(
+            settings.value(
+                f"{settings_class}/SamplingDistanceUnits",
+                QgsUnitTypes.toString(Qgis.DistanceUnit.Meters),
+                section=QgsSettings.Section.Plugins,
+            )
+        )
+
+        with QSignalBlocker(self._sampling_distance):
+            if success:
+                self._sampling_distance.set_units(unit)
+            else:
+                self._sampling_distance.set_units(Qgis.DistanceUnit.Meters)
+
+        with QSignalBlocker(self._angle_step):
+            self._angle_step.setValue(
+                settings.value(f"{settings_class}/AngleStep", 1, type=float, section=QgsSettings.Section.Plugins)
+            )
+
+        with QSignalBlocker(self._angle_difference):
+            self._angle_difference.setValue(
+                settings.value(f"{settings_class}/AngleDifference", 10, type=float, section=QgsSettings.Section.Plugins)
+            )
+
+    def save_settings(self) -> None:
+        settings = QgsSettings()
+        settings_class = f"{PluginConstants.settings_group}/CreateLoSTool"
+        settings.setValue(f"{settings_class}/LoSType", self.los_type, section=QgsSettings.Section.Plugins)
+        settings.setValue(f"{settings_class}/ObserverOffset", self.observer_offset, section=QgsSettings.Section.Plugins)
+        settings.setValue(f"{settings_class}/TargetOffset", self.target_offset, section=QgsSettings.Section.Plugins)
+        settings.setValue(
+            f"{settings_class}/SamplingDistance", self.sampling_distance.distance(), section=QgsSettings.Section.Plugins
+        )
+        settings.setValue(
+            f"{settings_class}/SamplingDistanceUnits",
+            QgsUnitTypes.encodeUnit(self._sampling_distance.unit()),
+            section=QgsSettings.Section.Plugins,
+        )
+        settings.setValue(f"{settings_class}/AngleStep", self.angle_step, section=QgsSettings.Section.Plugins)
+        settings.setValue(
+            f"{settings_class}/AngleDifference", self.angle_difference, section=QgsSettings.Section.Plugins
+        )
