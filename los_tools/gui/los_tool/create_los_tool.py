925c02a435dabff08c79e494fe6f942dd32b6d61
Jan Caha <jan.caha@outlook.com>
2025-01-13 09:18:40 +0100

    not needed

--- los_tools/gui/los_tool/create_los_tool.py
+++ los_tools/gui/los_tool/create_los_tool.py
@@ -31,8 +31,6 @@ class CreateLoSMapTool(LoSDigitizingToolWithWidget):
         super().clean()
         if self._widget:
             self._widget.setAddLoSEnabled(False)
-        self._start_point = None
-        self._last_towards_point = None
 
     def canvasReleaseEvent(self, e: QgsMapMouseEvent) -> None:
         if e.button() == Qt.RightButton and self._start_point is None and self._los_rubber_band.size() == 0:
